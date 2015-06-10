1f28773c16814d42e9cc738329bc3c2ec440b600
NickBrisebois <nickbrisebois12@gmail.com>
2015-06-10 19:01:42 -0400

    Started upload thing

--- app/app.py
+++ app/app.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python
 
 from flask import Flask, url_for, render_template, request,\
-        redirect, abort, session, g, flash, Markup
+        redirect, abort, session, g, flash
 from app.models import *
 
 app = Flask(__name__)
 
+#Set the folder for uploads
+UPLOAD_FOLDER = '/uploads'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOADE_FOLDER'],
+            filename)
+
+
 @app.route('/')
 def index():
-    return "I am root"
+    return render_template('index.html')
 
 
