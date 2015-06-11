e63c7543476145ae75c90e39c3c77b2c3add261a
hogofwar <hogofwar25@gmail.com>
2015-06-12 00:41:53 +0100

    Basic extension checking. Probably fails if there is no period.

--- app/app.py
+++ app/app.py
@@ -7,13 +7,17 @@ from app.models import *
 app = Flask(__name__)
 
 # Set the folder for uploads
+ext = set(['png', 'jpg', 'jpeg', 'webp', 'bmp', 'gif'])
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         f = request.files['fileToUpload']
-        f.save('./uploads/' + secure_filename(f.filename))
-        return "Uploaded"
+        if f.filename.rsplit('.', 1)[1] in ext:
+            f.save('./uploads/' + secure_filename(f.filename))
+            return "Uploaded"
+        else:
+            return "Not right ext"
 
 
 @app.route('/')
