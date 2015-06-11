1f74fd7796ad4718746abb92ce69e8d1a5dee9c9
hogofwar <hogofwar25@gmail.com>
2015-06-11 23:18:35 +0100

    Updated app.py to support actual, basic uploading.

--- app/app.py
+++ app/app.py
@@ -1,26 +1,21 @@
 #!/usr/bin/env python
 
-from flask import Flask, url_for, render_template, request,\
-        redirect, abort, session, g, flash
+from flask import Flask, url_for, render_template, request, redirect, abort, session, g, flash
+from werkzeug import secure_filename
 from app.models import *
 
 app = Flask(__name__)
 
-#Set the folder for uploads
-UPLOAD_FOLDER = '/uploads'
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+# Set the folder for uploads
 
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOADE_FOLDER'],
-            filename)
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['fileToUpload']
+        f.save('./uploads/' + secure_filename(f.filename))
+        return "Uploaded"
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-
