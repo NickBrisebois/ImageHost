932ca99414dc967cd2e4e53c02edd7978082d4e5
hogofwar <hogofwar25@gmail.com>
2015-06-13 17:40:28 +0100

    Added 404 support.

--- app/app.py
+++ app/app.py
@@ -23,3 +23,7 @@ def upload_file():
 @app.route('/')
 def index():
     return render_template('index.html')
+    
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
